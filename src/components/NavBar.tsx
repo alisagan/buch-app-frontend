cc22b29473fb1479522cee50833cecbc5118cb99
Alisa Ganter <gaal1019@h-ka.de>
2025-06-06 20:30:09 +0200

    NavBar angepasst und Burgermenü hinzugefügt

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,41 +7,28 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = [
-  { label: "Anlegen", key: "anlegen" },
-  { label: "Suchen", key: "suchen" },
+const settings = [
+  { label: 'Suchen', key: 'suchen' },
+  { label: 'Anlegen', key: 'anlegen' },
+  { label: 'Ändern', key: 'ändern' },
+  { label: 'Löschen', key: 'löschen' },
 ] as const;
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
 type NavBarProps = {
-  onSeiteWechsel: (seite: "anlegen" | "suchen") => void;
+  onSeiteWechsel: (seite: 'anlegen' | 'suchen' | 'ändern' | 'löschen') => void;
 };
 
 function NavBar({ onSeiteWechsel }: NavBarProps) {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null,
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
@@ -50,7 +37,7 @@ function NavBar({ onSeiteWechsel }: NavBarProps) {
     <AppBar position="static" color="primary">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -58,83 +45,43 @@ function NavBar({ onSeiteWechsel }: NavBarProps) {
             href="#"
             sx={{
               mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
             }}
           >
             LOGO
           </Typography>
 
-          {/* Mobile Menü */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
+          <Box sx={{ flexGrow: 1 }} />
+
+          {/* Burgermenü */}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="User actions">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <MenuIcon/>
+              </IconButton>
+            </Tooltip>
             <Menu
-              anchorEl={anchorElNav}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-              transformOrigin={{ vertical: "top", horizontal: "left" }}
-              sx={{ display: { xs: "block", md: "none" } }}
+              sx={{ mt: '45px' }}
+              anchorEl={anchorElUser}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
             >
-              {pages.map(({ label, key }) => (
+             {settings.map(({ label, key }) => (
                 <MenuItem
                   key={key}
                   onClick={() => {
-                    handleCloseNavMenu();
+                    handleCloseUserMenu();
                     onSeiteWechsel(key);
                   }}
                 >
                   <Typography textAlign="center">{label}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          {/* Desktop Menü */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map(({ label, key }) => (
-              <Button
-                key={key}
-                onClick={() => {
-                  handleCloseNavMenu();
-                  onSeiteWechsel(key);
-                }}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {label}
-              </Button>
-            ))}
-          </Box>
-
-          {/* Benutzer-Menü */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              anchorEl={anchorElUser}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-              anchorOrigin={{ vertical: "top", horizontal: "right" }}
-              transformOrigin={{ vertical: "top", horizontal: "right" }}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
             </Menu>
